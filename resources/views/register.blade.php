9593ca64196d76c6a4d1b3d75e040988571d3baf
arafat <anarafat.com>
2023-11-19 13:08:19 +0600

    Assignment 01 complete

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -128,7 +128,7 @@
                 required
                 class="block w-full rounded-md border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6" />
             </div>
-          </div>
+        </div>
 
         <div>
           <button
