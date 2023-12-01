d12ca79331b843538d05af56d8aa24adc5be5160
arafat <anarafat.com>
2023-12-01 22:49:16 +0600

    Query optimization techniques tried

--- app/Services/CommentService.php
+++ app/Services/CommentService.php
@@ -12,7 +12,7 @@ class CommentService{
     public function store(Array $data, Post $post){
         $success = DB::table('comments')->insert([
             ...$data,
-            'post_id'=>$post->id,
+            'post_id'=> $post->id,
             'user_id'=>Auth::user()->id,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use App\Models\User;
+use Illuminate\Database\Query\Builder;
 
 class PostService{
 
@@ -49,29 +50,31 @@ class PostService{
 
 
     public function postsWithCommentsCount(){
-        $posts = DB::table('posts')
-                    ->join('users', 'users.id', 'posts.user_id')
-                    ->select('posts.*', 'users.id as userId', 'users.uuid as userUuid', 'users.firstName', 'users.lastName', 'users.email')
-                    ->orderBy('posts.id', 'DESC')
-                    ->get();
+        // $posts = DB::table('posts')
+        //             ->join('users', 'users.id', 'posts.user_id')
+        //             ->select('posts.*', 'users.id as userId', 'users.firstName', 'users.lastName', 'users.email',
+        //                 DB::raw('(SELECT COUNT(id) FROM comments WHERE comments.post_id = posts.id GROUP BY post_id) as comments_count')
+        //             )
+        //             ->orderBy('posts.id', 'DESC')
+        //             ->get();
 
-        $posts = collect($posts)->map(function ($item){
-            $item->comments_count = DB::table('comments')->where('post_id', $item->id)->count();
-            return $item;
-        });
+        // $posts = collect($posts)->map(function ($item){
+        //     $item->comments_count = DB::table('comments')->where('post_id', $item->id)->count();
+        //     return $item;
+        // });
 
 
         // foreach($posts as $post){
         //     $post->comments_count = DB::table('comments')->where('post_id', $post->id)->count();
         // }
 
-        // $posts = DB::table('posts')
-        //             ->select('posts.*', 'users.id as userId', 'users.firstName', 'users.lastName', 'users.email', DB::raw('count(comments.id) as comments_count'))
-        //             ->join('users', 'users.id', 'posts.user_id')
-        //             ->leftJoin('comments', 'posts.id', '=', 'comments.post_id')
-        //             ->groupBy('posts.id')
-        //             ->orderBy('posts.id', 'DESC')
-        //             ->get();
+        $posts = DB::table('posts')
+                    ->select('posts.*', 'users.id as userId', 'users.firstName', 'users.lastName', 'users.email', DB::raw('count(comments.id) as comments_count'))
+                    ->join('users', 'users.id', 'posts.user_id')
+                    ->leftJoin('comments', 'posts.id', '=', 'comments.post_id')
+                    ->groupBy('posts.id')
+                    ->orderBy('posts.id', 'DESC')
+                    ->get();
 
         return $posts;
 
